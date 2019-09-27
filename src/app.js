f95d2e2f84ba9b54d3005a8c4b002dbce72bce7f
AlexKale <alex-kale@hotmail.com>
2019-09-27 15:19:21 +0300

    Update default state

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import Store from './redux/Store';
 import repoBrowserReducer from './components/RepoBrowser/RepoBrowserReducer';
 import RepoBrowserView from './components/RepoBrowser/RepoBrowserView';
 
-const store = new Store(repoBrowserReducer);
-
-const repoBrowserView = new RepoBrowserView(document.querySelector('.RepoBrowserView'), store);
+document.addEventListener('DOMContentLoaded', () => {
+  const store = new Store(repoBrowserReducer);
+  const repoBrowserView = new RepoBrowserView(document.querySelector('.RepoBrowserView'), store);
+});
--- src/components/RepoBrowser/RepoBrowserReducer.js
+++ src/components/RepoBrowser/RepoBrowserReducer.js
@@ -1,5 +1,4 @@
 const defaultState = {
-  count: 0,
   files: [
     {
       id: 0,
@@ -12,6 +11,66 @@ const defaultState = {
       name: 'ci',
       type: 'folder',
       commit: 'c53dsv',
+    },
+    {
+      id: 2,
+      name: 'contrib',
+      type: 'folder',
+      commit: 's53dsv',
+    },
+    {
+      id: 3,
+      name: 'http',
+      type: 'folder',
+      commit: 'h5jdsv',
+    },
+    {
+      id: 4,
+      name: 'lib',
+      type: 'folder',
+      commit: 'f5jdsv',
+    },
+    {
+      id: 5,
+      name: 'local',
+      type: 'folder',
+      commit: 'k5jdsv',
+    },
+    {
+      id: 6,
+      name: 'packages',
+      type: 'folder',
+      commit: 'a5jdsv',
+    },
+    {
+      id: 7,
+      name: 'robots',
+      type: 'folder',
+      commit: 'l5jdsv',
+    },
+    {
+      id: 8,
+      name: 'server',
+      type: 'folder',
+      commit: 'j5jdsv',
+    },
+    {
+      id: 9,
+      name: 'ut',
+      type: 'folder',
+      commit: '5jdsvk',
+    },
+    {
+      id: 10,
+      name: 'README.md',
+      type: 'text',
+      commit: 'h5jdsl',
+    },
+    {
+      id: 11,
+      name: 'ya.make',
+      type: 'code',
+      commit: 'k5jdsv',
     },
   ],
   commits: {
@@ -26,16 +85,71 @@ const defaultState = {
       message: '[vcs] test for empty commit message',
       author: 'nikitxskv',
       time: '1 min ago',
-    }
+    },
+    s53dsv: {
+      hash: 's53dsv',
+      message: '[vcs] change owner to g:arc',
+      author: 'nalpp',
+      time: '16:25',
+    },
+    h5jdsv: {
+      hash: 'h5jdsv',
+      message: '[vcs] move http to arc',
+      author: 'somov',
+      time: 'Yesterday, 14:50',
+    },
+    f5jdsv: {
+      hash: 'f5jdsv',
+      message: 'ARCADIA-771: append /trunk/arcadia/',
+      author: 'deshevoy',
+      time: 'Jan 11, 12:01',
+    },
+    k5jdsv: {
+      hash: 'k5jdsv',
+      message: '[vcs] move http to arc',
+      author: 'mvel',
+      time: 'Dec 29, 2017',
+    },
+    a5jdsv: {
+      hash: 'a5jdsv',
+      message: '[vcs] VCS-803: packages for services',
+      author: 'levanov',
+      time: 'Jan 1, 12:01',
+    },
+    l5jdsv: {
+      hash: 'l5jdsv',
+      message: 'ARCADIA-771: convert string to json object',
+      author: 'torkve',
+      time: 'Dec 29, 2017',
+    },
+    j5jdsv: {
+      hash: 'j5jdsv',
+      message: '[vcs] get list of refs',
+      author: 'spreis',
+      time: 'Dec 29, 2017',
+    },
+    '5jdsvk': {
+      hash: '5jdsvk',
+      message: '[vsc] store merge conflicts',
+      author: 'annaveronika',
+      time: 'Dec 29, 2017',
+    },
+    h5jdsl: {
+      hash: 'h5jdsl',
+      message: '[vcs] add readme',
+      author: 'pg',
+      time: 'Dec 29, 2017',
+    },
   },
-  shownFiles: [0, 1],
+  shownFiles: Array.from(Array(12).keys()),
 };
 
 const repoBrowserReducer = (action, state = defaultState) => {
   console.log('repoBrowserReducer called for action', action);
 
-  switch (action) {
+  switch (action.type) {
     case '@@init':
+      console.log('Setting default state', defaultState);
       return defaultState;
     default:
       return state;
