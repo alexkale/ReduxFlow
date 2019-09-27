675471c539b3229036528774fb4606e6f771baaa
AlexKale <alex-kale@hotmail.com>
2019-09-27 18:04:04 +0300

    handle enter keypress

--- src/app.js
+++ src/app.js
@@ -5,8 +5,24 @@ import SearchView from './components/Search/SearchView';
 
 import rootReducer from './rootReducer';
 
+const KEY_CODES = {
+  'ENTER': 13,
+};
+
+/**
+ * App setup
+ */
 document.addEventListener('DOMContentLoaded', () => {
   const store = new Store(rootReducer);
   const repoBrowserView = new RepoBrowserView(document.querySelector('.RepoBrowser'), store);
   const searchView = new SearchView(document.querySelector('.Search'), store);
+
+  document.body.addEventListener('keypress', (event) => {
+    if (event.keyCode === KEY_CODES.ENTER) {
+      const { nextElementSibling } = event.target;
+      if (nextElementSibling && nextElementSibling.tagName === 'BUTTON') {
+        nextElementSibling.click();
+      }
+    }
+  });
 });
